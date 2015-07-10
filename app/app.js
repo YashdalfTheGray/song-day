42308ef21667588c939c96e8880c79bf47004d41
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-07-09 21:39:22 -0500

    moved to material-design-icons and started using cdns for 3rd party components

--- app/app.js
+++ app/app.js
@@ -8,27 +8,8 @@ angular.module('songADay',
 ).config([
     '$urlRouterProvider',
     '$stateProvider',
-    '$mdIconProvider',
     '$mdThemingProvider',
-    function($urlRouterProvider, $stateProvider, $mdIconProvider, $mdThemingProvider) {
-
-        $mdIconProvider
-            .iconSet('action', './assets/action-icons.svg', 24)
-            .iconSet('alert', './assets/alert-icons.svg', 24)
-            .iconSet('av', './assets/av-icons.svg', 24)
-            .iconSet('communication', './assets/communication-icons.svg', 24)
-            .iconSet('content', './assets/content-icons.svg', 24)
-            .iconSet('device', './assets/device-icons.svg', 24)
-            .iconSet('editor', './assets/editor-icons.svg', 24)
-            .iconSet('file', './assets/file-icons.svg', 24)
-            .iconSet('hardware', './assets/hardware-icons.svg', 24)
-            .iconSet('icons', './assets/icons-icons.svg', 24)
-            .iconSet('image', './assets/image-icons.svg', 24)
-            .iconSet('maps', './assets/maps-icons.svg', 24)
-            .iconSet('navigation', './assets/navigation-icons.svg', 24)
-            .iconSet('notification', './assets/notification-icons.svg', 24)
-            .iconSet('social', './assets/social-icons.svg', 24)
-            .iconSet('toggle', './assets/toggle-icons.svg', 24);
+    function($urlRouterProvider, $stateProvider, $mdThemingProvider) {
 
         $mdThemingProvider.theme('default')
                           .primaryPalette('blue')
