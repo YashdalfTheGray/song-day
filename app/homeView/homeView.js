43a3aaf692e255bc7dcc53918ef7b1f2b618f82c
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-07-12 14:04:26 -0500

    removed commented code

--- app/homeView/homeView.js
+++ app/homeView/homeView.js
@@ -15,39 +15,6 @@ angular.module('songADay')
                 $scope.contentLoaded = true;
             });
 
-            // $scope.testData = [
-            //     {
-            //         date: '1436649308',
-            //         title: 'Test song title 1',
-            //         artist: 'Some artist',
-            //         link: 'http://www.google.com'
-            //     },
-            //     {
-            //         date: '1436562908',
-            //         title: 'Test song title 2'
-            //     },
-            //     {
-            //         date: '1436476508',
-            //         title: 'Test song title 3'
-            //     },
-            //     {
-            //         date: '1436390108',
-            //         title: 'Test song title 4'
-            //     },
-            //     {
-            //         date: '1436303708',
-            //         title: 'Test song title 5'
-            //     },
-            //     {
-            //         date: '1436217308',
-            //         title: 'Test song title 6'
-            //     },
-            //     {
-            //         date: '1436130908',
-            //         title: 'Test song title 7'
-            //     }
-            // ];
-
             $scope.formatDate = function formatDate (unixDate) {
                 return moment(unixDate, 'X').calendar();
             };
