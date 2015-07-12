fd61c5a88ea37e41d44ef14bc69a8459a1fc0cca
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-07-12 15:10:02 -0500

    refactored toolbar buttons and configured visibility

--- app/toolbar/toolbar.js
+++ app/toolbar/toolbar.js
@@ -5,28 +5,13 @@ angular.module('songADay')
     [
         '$scope', '$state',
         function ($scope, $state) {
-            $scope.refreshPage = function refreshPage () {
-                console.log('Page refreshed!');
+            $scope.stateIs = function stateIs(state) {
+                return $state.is(state);
             };
 
-            $scope.stateIsHome = function stateIsHome() {
-                return $state.is('home');
+            $scope.goTo = function goToState(state) {
+                $state.go(state);
             };
-
-            $scope.addSong = function addSong() {
-                $state.go('addsong');
-            };
-
-            $scope.showAbout = function showAbout() {
-                $state.go('about');
-            };
-
-            $scope.$root.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-                if (fromState.name === 'addsong' && toState.name === 'home' && $scope.$root.songAdded) {
-                    $scope.refreshPage();
-                    $scope.$root.songAdded = false;
-                }
-            });
         }
     ]
 );
