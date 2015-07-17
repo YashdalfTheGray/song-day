1f306ae4c52413906f4f3a1956e27dab376a4f24
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-07-16 23:37:58 -0500

    wired up song editor

--- app/homeView/homeView.js
+++ app/homeView/homeView.js
@@ -3,8 +3,8 @@
 angular.module('songADay')
 .controller('HomeViewCtrl', 
     [
-        '$scope', '$window', '$firebaseArray', 'songSvc',
-        function ($scope, $window, $firebaseArray, songSvc) {
+        '$scope', '$window', '$firebaseArray', '$mdDialog', 'songSvc',
+        function ($scope, $window, $firebaseArray, $mdDialog, songSvc) {
             var ref = new Firebase('https://onesongaday.firebaseio.com/songs');
             var songs = $firebaseArray(ref);
 
@@ -26,8 +26,44 @@ angular.module('songADay')
                 $window.open(link, '_blank');
             };
 
-            $scope.editSongDetails = function editSongDetails() {
-                // this is for future implementations
+            $scope.editSongDetails = function editSongDetails(ev, songObj) {
+                $mdDialog.show({
+                      controller: EditDialogCtrl,
+                      templateUrl: 'homeView/editDialog.tpl.html',
+                      parent: angular.element(document.body),
+                      targetEvent: ev,
+                      locals: {
+                         song: _.assign({}, songObj)
+                      }
+                })
+                .then(function (result) {
+                    if(result) {
+                        songObj.title = result.title;
+                        songObj.artist = result.artist;
+                        songObj.genre = result.genre;
+                        songObj.link = result.link;
+                        songs.$save(songObj)
+                        .then(function (result) {
+                            console.log('Record with id ' + result.key() + ' was saved to Firebase.');
+                        });
+                    }
+                });
+                 
+                function EditDialogCtrl ($scope, $mdDialog, song) {
+                    $scope.song = song;
+
+                    $scope.cancel = function cancelDialog() {
+                        $mdDialog.cancel();
+                    };
+
+                    $scope.hide = function hideDialog() {
+                        $mdDialog.hide();
+                    };
+
+                    $scope.saveSong = function SaveSong(song) {
+                        $mdDialog.hide(song);
+                    };
+                }
             };
 
             $scope.getVideoId = function getVideoId (link) {
